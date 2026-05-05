17ad8944f775f9b50d209b1c16ec93d81384f975
Tushar Islam <tushar060703@gmail.com>
2026-05-05 09:15:27 -0400

    Made updates to the Room class for exercise 2

--- src/main/java/com/pluralsight/models/Room.java
+++ src/main/java/com/pluralsight/models/Room.java
@@ -14,6 +14,40 @@ public class Room
         this.isDirty = isDirty;
     }
 
+    public void checkIn() {
+        if (isOccupied()) {
+            System.out.println("Cannot Check In, the room is currently occupied.");
+            return;
+        }
+        if (isDirty()) {
+            System.out.println("Cannot Check In, the room is currently dirty.");
+        }
+        this.isDirty = true;
+        this.isOccupied = true;
+        System.out.println("Checked In, the room is now occupied.");
+    }
+
+    public void checkout() {
+        if (!isOccupied()) {
+            System.out.println("Cannot Check Out, the room is not occupied.");
+            return;
+        }
+        this.isOccupied = false;
+        System.out.println("Checked Out, the room is now unoccupied.");
+    }
+
+    public void cleanRoom() {
+        if (isOccupied()) {
+            System.out.println("Cannot clean room, it is currently occupied.");
+            return;
+        }
+        if (!isDirty()) {
+            System.out.println("Cannot clean room, it is already cleaned.");
+        }
+        this.isDirty = false;
+        System.out.println("Room Cleaned, it is now available.");
+    }
+
     public boolean isAvailable() {
         return !isOccupied && !isDirty;
     }
